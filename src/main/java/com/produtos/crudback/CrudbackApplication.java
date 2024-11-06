2645f3cb46ca2d844b56b59982c9b2a3f636bdb9
brenoloa <contatobrenoloa@gmail.com>
2024-11-05 23:26:27 -0300

    criacao do projeto

---
+++ src/main/java/com/produtos/crudback/CrudbackApplication.java
@@ -0,0 +1,15 @@
+package com.produtos.crudback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudbackApplication.class, args);
+
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/main/java/com/produtos/crudback/controller/testeApiController.java
@@ -0,0 +1,18 @@
+package com.produtos.crudback.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class testeApiController {
+
+    @GetMapping
+    public String teste(){
+
+        return "GET ok,! ";
+
+    }
+}
